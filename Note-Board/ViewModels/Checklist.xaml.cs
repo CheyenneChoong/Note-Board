ac3fecba6550186db1519ac01f2616e60e6eb830
Cheyenne <cheyennechoong@gmail.com>
2026-08-15 18:04:14 +0800

    Draggable components.

--- Note-Board/ViewModels/Checklist.xaml.cs
+++ Note-Board/ViewModels/Checklist.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using Note_Board.Helpers;
 
 namespace Note_Board.Views;
 public partial class Checklist : UserControl
@@ -6,5 +7,6 @@ public partial class Checklist : UserControl
     public Checklist()
     {
         InitializeComponent();
+        Loaded += (s, e) => Drag.EnableDrag(this, (Parent as Canvas)!);
     }
 }
--- Note-Board/ViewModels/Note.xaml.cs
+++ Note-Board/ViewModels/Note.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using Note_Board.Helpers;
 
 namespace Note_Board.Views;
 public partial class Note : UserControl
@@ -6,5 +7,6 @@ public partial class Note : UserControl
     public Note()
     {
         InitializeComponent();
+        Loaded += (s, e) => Drag.EnableDrag(this, (Parent as Canvas)!);
     }
 }
